a0c1c618ee801e3dd21504345fcfe4e61e9b4ba4
Alzpetka <lili3.pokorna@seznam.cz>
2021-09-14 12:49:53 +0200

    dependencies

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 readme = open('README.md').read()
 history = open('CHANGES.md').read()
-OAREPO_VERSION = os.environ.get('OAREPO_VERSION', '3.3.0')
+OAREPO_VERSION = os.environ.get('OAREPO_VERSION', '3.3.40')
 
 
 install_requires = [
